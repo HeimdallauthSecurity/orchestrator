1fc9b390ac68d8be0947e58c1f28e36364271885
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-25 22:48:26 -0600

    init(core): Commit to create project files

---
+++ src/main/java/com/heimdallauth/utils/heimdallorchestrator/HeimdallOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.heimdallauth.utils.heimdallorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeimdallOrchestratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heimdallauth/utils/heimdallorchestrator/HeimdallOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heimdallauth.utils.heimdallorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimdallOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
